df1c1e516b61f579b587dc8126c4d74d111b7444
karankataria <karankatariac22@gmail.com>
2023-10-23 22:17:44 -0400

    index file updated

--- index.js
+++ index.js
@@ -5,19 +5,9 @@ require("dotenv").config();
 const { getExpenses } = require("./splitwiseAPI");
 const { getNotification } = require("./getNotification");
 
-// Define a route for the home page
-app.get("/", (req, res) => {
-  res.send("Welcome to the Home Page");
-});
-
 // getExpenses();
 
 getNotification();
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
 
 // to dos
 //  add group name
